8beb0bce975b22580f4cb17d271aedfd40c2ef34
Yong <ycho97@uwo.ca>
2020-11-21 00:59:12 -0500

    dead page is done

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import DeadPage from './containers/DeadPage';
+
 function App() {
   return (
     <div className="App">
-
+       <BrowserRouter>
+          <Switch>
+             <Route path='/' component={DeadPage}/>
+          </Switch>
+       </BrowserRouter>
     </div>
   );
 }
