14025899fe76be2e33aca7bf6712d82faf689a0c
Yong <ycho97@uwo.ca>
2020-11-20 23:49:43 -0500

    auth on backend done

---
+++ server/controllers/user.js
@@ -0,0 +1,118 @@
+import User from '../models/user';
+import bcrypt from 'bcrypt';
+
+export const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    let user;
+    const errors = [];
+    let field;
+
+    if(username.includes('@')){
+        user = await User.findOne({ email : username});
+        field = 'Email';
+    } else{ 
+        user = await User.findOne({username});
+        field = 'Username';
+    }
+
+    if(!user){
+        errors.push({
+            field,
+            message: `${field} doesn't exist`
+        });
+    }
+
+    else{
+        const valid = await bcrypt.compare(password, user.password);
+
+        if(valid){
+            user.password = '';
+            req.session.uid = user._id;
+        } else{
+            errors.push({
+                field: 'Password',
+                message: 'Incorrect password'
+            });
+
+            user = null;
+        }
+    }
+
+    res.json({user, errors});
+}
+
+export const register = async (req, res) => {
+    const { username, password, email } = req.body;
+
+    const errors = [];
+    let user;
+
+    if(username.includes('@')){
+        errors.push({
+            field: 'Username',
+            message: 'Username cannot include the @ sign'
+        });
+    }
+
+    if(!email.includes('@')){
+        errors.push({
+            field: 'Email',
+            message: 'Email should include the @ sign'
+        });
+    }
+
+    user = await User.findOne({ email });
+
+    if(user){
+        errors.push({
+            field: 'Email',
+            message: 'Email is already taken'
+        });
+
+        user = null;
+    }
+
+    user = await User.findOne({ username });
+
+    if(user){
+        errors.push({
+            field: 'Username',
+            message: 'Username is already taken'
+        });
+
+        user = null;
+    }
+
+    if(errors.length === 0){
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hashSync(password, salt);
+
+        const newUser = new User({...req.body, password: hashedPassword});
+
+        user = await newUser.save();
+        user.password = '';
+
+        req.session.uid = user._id;
+    }
+
+    res.json({ user, errors });
+}
+
+export const logout = async (req, res) => {
+    await req.session.destroy(err => {
+        res.clearCookie(process.env.COOKIE_NAME);
+
+        if(err) {
+            res.json({message: 'Something went wrong'});
+        }
+
+        else{
+            res.json({message: 'Sucessfuly signed out'});
+        }
+    });
+}
+
+export const forgotPassword = async (req, res) => {
+    
+}
