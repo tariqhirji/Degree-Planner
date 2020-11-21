db720b859a03ca1d58a1b345365e46185d07cca1
Tariq Hirji <tariqhirji15@gmail.com>
2020-11-21 01:17:53 -0500

    finished homepage and navbar

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,15 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
+import Landing from './containers/Landing.jsx'
+
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Navbar />
+        <Landing />
+      </BrowserRouter>
     </div>
   );
 }
